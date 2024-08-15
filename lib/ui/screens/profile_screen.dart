7c6d2ca50d12c43b0b14c2138968c21f9469bca7
Hunter20050910 <145260700+Abrorov-javohir@users.noreply.github.com>
2024-08-15 19:42:58 +0500

    profile screen o'zgartirildi

--- lib/ui/screens/profile_screen.dart
+++ lib/ui/screens/profile_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:retsept_cherno/ui/screens/tab_box_screen.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:retsept_cherno/ui/widgets/followers_and_circle_avatar_following_widget.dart';
 import 'package:retsept_cherno/ui/widgets/silver_fill_reamining_widget.dart';
-import 'package:share_plus/share_plus.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -24,14 +23,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   void _shareProfile() {
-    // Profil URL-manzili
     final String profileUrl = 'https://t.me/Flutter_with_Javohir';
-
-    // Profilni ulashish
-
     Share.share('Check out this amazing chef profile!\n$profileUrl');
-
-    // Profil sahifasiga o'tish
     Navigator.pushNamed(context, '/profile');
   }
 
@@ -45,47 +38,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.arrow_upward),
-              onPressed: _scrollToTop,
-            ),
-          ],
-        ),
-        body: CustomScrollView(
-          controller: _scrollController,
-          slivers: [
-            SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  FollowersAndCircleAvatarFollowingWidget(
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          body: CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              SliverAppBar(
+                expandedHeight: 500,
+                pinned: true,
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_upward),
+                    onPressed: _scrollToTop,
+                  ),
+                ],
+                flexibleSpace: FlexibleSpaceBar(
+                  background: FollowersAndCircleAvatarFollowingWidget(
                     isFollowing: isFollowing,
                     followingCount: followingCount,
                     toggleFollow: _toggleFollow,
                     shareProfile: () => _shareProfile(),
                   ),
-                  const SizedBox(height: 24.0),
-                  const TabBar(
-                    labelColor: Colors.black,
-                    unselectedLabelColor: Colors.grey,
-                    tabs: [
-                      Tab(text: "Posts"),
-                      Tab(text: 'Less Details'),
-                    ],
-                  ),
-                ],
+                ),
+                bottom: const TabBar(
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  tabs: [
+                    Tab(text: "Posts"),
+                    Tab(text: 'Less Details'),
+                  ],
+                ),
               ),
-            ),
-            const SilverFillReaminingWidget(),
-          ],
+              const SilverFillReaminingWidget(),
+            ],
+          ),
         ),
-       
       ),
     );
   }
