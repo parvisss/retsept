98c95d74a40819b333f6e91534f83f9a0fd87930
Hunter20050910 <145260700+Abrorov-javohir@users.noreply.github.com>
2024-08-16 12:19:04 +0500

    korusel qo'shildi homescreenga

--- lib/ui/screens/home_Screen.dart
+++ lib/ui/screens/home_Screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:retsept_cherno/bloc/retsept/retsept_bloc.dart';
@@ -6,6 +7,7 @@ import 'package:retsept_cherno/bloc/retsept/retsept_state.dart';
 import 'package:retsept_cherno/bloc/user/user_bloc.dart';
 import 'package:retsept_cherno/bloc/user/user_event.dart';
 import 'package:retsept_cherno/services/firestore/retsept/retsept_firebase.dart';
+import 'package:retsept_cherno/ui/widgets/home/carusel_item.dart';
 import 'package:retsept_cherno/ui/widgets/home/classic_victoria.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,12 +18,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
   @override
   void initState() {
+    super.initState();
     context.read<RetseptBloc>().add(LoadRetsepts());
     context.read<UserBloc>().add(LoadUserDataEvent());
 
-    super.initState();
+    Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      if (_currentPage < 2) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -46,32 +70,72 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: BlocBuilder<RetseptBloc, RetseptState>(
-                builder: (context, state) {
-              if (state is RetseptLoading) {
+              builder: (context, state) {
+                if (state is RetseptLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state is RetseptError) {
+                  return Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(state.message),
+                    ),
+                  );
+                }
+                if (state is RetseptLoaded) {
+                  return ListView(
+                    children: [
+                      // CaruselItemlarni ko'rsatish
+                      SizedBox(
+                        height: 350.0,
+                        child: PageView(
+                          controller: _pageController,
+                          children: const [
+                            CaruselItem(
+                              imagePath: 'assets/food2.png',
+                              title: 'Strawberry',
+                            ),
+                            CaruselItem(
+                              imagePath: 'assets/food3.png',
+                              title: 'Berry',
+                            ),
+                            CaruselItem(
+                              imagePath: 'assets/food10.png',
+                              title: 'Egg',
+                            ),
+                            CaruselItem(
+                              imagePath: 'assets/food5.png',
+                              title: 'Meat',
+                            ),
+                            CaruselItem(
+                              imagePath: 'assets/food6.png',
+                              title: 'Strawberry cake',
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16.0,
+                      ),
+                      // Retseptlar ro'yxati
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: state.props.length,
+                        itemBuilder: (ctx, index) {
+                          final retsetsept = state.props[index];
+                          return ClassicVictoria(retsept: retsetsept);
+                        },
+                      ),
+                    ],
+                  );
+                }
                 return const Center(
-                  child: CircularProgressIndicator(),
+                  child: Text("Empty data"),
                 );
-              } else if (state is RetseptError) {
-                return Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(state.message),
-                  ),
-                );
-              }
-              if (state is RetseptLoaded) {
-                return ListView.builder(
-                  itemCount: state.props.length,
-                  itemBuilder: (ctx, index) {
-                    final retsetsept = state.props[index];
-                    return ClassicVictoria(retsept: retsetsept);
-                  },
-                );
-              }
-              return const Center(
-                child: Text("Empty data"),
-              );
-            }),
+              },
+            ),
           ),
         ),
       ),
--- lib/ui/widgets/home/carusel_item.dart
+++ lib/ui/widgets/home/carusel_item.dart
@@ -14,6 +14,8 @@ class CaruselItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(5.0),
+      width: 350.0,
+      height: 350.0,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
         image: DecorationImage(
